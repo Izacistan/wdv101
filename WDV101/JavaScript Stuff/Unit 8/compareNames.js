7676773f84b66b22a6ef0e8b2e5c7511753c8188
Isaac Hillaker <izacistan@protonmail.com>
2023-11-06 13:26:40 -0600

    Updated with input validation for names.

--- WDV101/JavaScript Stuff/Unit 8/compareNames.js
+++ WDV101/JavaScript Stuff/Unit 8/compareNames.js
@@ -3,31 +3,35 @@
   // 1. Create a function called compareNames
   function compareNames() {
     /** 
-     * This function grabs the values from the two form fields, converts them to uppercase for proper comparision,
+     * This function grabs the values from the two form fields, converts them to uppercase for proper comparison,
      * and then checks if the names are the same, returning different results depending on the answer.
     */
 
     let name1 = document.getElementById("name1").value;
     let name2 = document.getElementById("name2").value;
 
-    // 3. Case insensitive numbers
+    // Input validation: Only letters and hyphens allowed
+    const validInputPattern = /^[A-Za-z \-]+$/;
+
+    if (!validInputPattern.test(name1) || !validInputPattern.test(name2)) {
+        // Input contains invalid characters
+        return "Invalid Input";
+    }
+
+    // Convert to uppercase for case-insensitive comparison
     name1 = name1.toUpperCase();
     name2 = name2.toUpperCase();
 
-    // 2. Compare two input values from the forms
+    // Compare two input values from the forms
     if (name1 === name2) {
-
-      // 4. Return Same
-      return "Same"
-
+        // Return Same
+        return "Same";
     } else {
-
-      // 4. Return Different
-      return "Different"
-
+        // Return Different
+        return "Different";
     }
+}
 
-  }
 
   function displayResult() {
     /** 
